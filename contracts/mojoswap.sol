70d5e93e05610ea86fa35a94eb8352ec27d7cd00
projamkiller <fletcher@jamkillerproductions.io>
2025-02-25 07:03:48 -0600

    Commit

--- contracts/mojoswap.sol
+++ contracts/mojoswap.sol
@@ -3,29 +3,34 @@ pragma solidity ^0.8.17;
 
 import "./mojo.sol";
 
+interface IPGKBurner {
+    function verifyBurn(address user, uint256 amount) external view returns (bool);
+}
+
 /**
  * @title MojoSwap
  * @notice Handles PJK burn events from Polygon and mints Mojo on Optimism.
  */
 contract MojoSwap {
-    Mojo public mojoToken;
+    Mojo public mojoToken = Mojo(0xf9e7D3cd71Ee60C7A3A64Fa7Fcb81e610Ce1daA5);
+    IPGKBurner public pjkBurner = IPGKBurner(0xFF197b807f60c2985938c0Fe920D89E1c80a1aF3);
     address public owner;
-    // Simplified: Assume an oracle or bridge sets this after verifying Polygon burn
+    
     mapping(address => uint256) public pendingSwaps;
 
     event SwapRequested(address indexed user, uint256 pjkAmount);
     event SwapCompleted(address indexed user, uint256 pjkAmount, uint256 mojoAmount);
 
-    constructor(address _mojoToken) {
-        mojoToken = Mojo(_mojoToken);
+    constructor() {
         owner = msg.sender;
     }
 
     /**
-     * @notice Request swap (called after burning PJK on Polygon).
-     * @param pjkAmount Amount burned, verified off-chain or via bridge.
+     * @notice Request swap after burning PJK on Polygon.
+     * @param pjkAmount Amount burned, verified via the PGKBurner contract.
      */
     function requestSwap(uint256 pjkAmount) external {
+        require(pjkBurner.verifyBurn(msg.sender, pjkAmount), "Burn verification failed");
         pendingSwaps[msg.sender] += pjkAmount;
         emit SwapRequested(msg.sender, pjkAmount);
     }
